be7c2a397723831b1ad48e898f5aa21f586081f3
Giacomo Nanni <gn.nanni@gmail.com>
2024-11-26 10:21:32 +0100

    local vs remote

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,29 +1,38 @@
 import { writable } from 'svelte/store';
 import { base } from '$app/paths';
+import * as config from '@setup';
 
 
 export const db = writable([]);
-
 export async function loadDb() {
     try {
-        const response = await fetch(`${base}/db.json`);
+        const url = config.local.includes('http') ? config.local : `${base}/${config.local}`;
+        console.log(`Fetching URL: ${url}`);
+        const response = await fetch(url);
+
         if (!response.ok) {
-            throw new Error(`Failed to load db.json: ${response.statusText}`);
+            throw new Error(`Failed to load db: ${response.statusText} (Status: ${response.status})`);
         }
+
+        const contentType = response.headers.get('content-type');
+        if (!contentType || !contentType.includes('application/json')) {
+            throw new Error('Response is not valid JSON');
+        }
+
         const data = await response.json();
-        db.set(data.map((d) => {
-            return {
+        db.set(
+            data.map((d) => ({
                 ...d,
-                // only for omeka
                 "@id": d["@id"]
                     ?.replace(/\/items\//, '/resources/')
                     ?.replace(/\/media\//, '/resources/')
                     ?.replace(/\/item_sets\//, '/resources/')
-            };
-        }));
-
+            }))
+        );
+        console.log('Database loaded successfully.');
 
     } catch (error) {
         console.error('Error loading db.json:', error);
     }
 }
+
